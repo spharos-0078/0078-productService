964e70df771bc0e7d553f49017e36b1dd66d4416
LeeYeongin <youngin5757@gmail.com>
2025-06-11 10:11:04 +0900

    [feat]: Swagger 추가
    - ProductService Swagger 추가
    
    #2

---
+++ src/main/java/com/pieceofcake/product_service/common/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.pieceofcake.product_service.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String BEARER_TOKEN_PREFIX = "Bearer";
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        String securityJwtName = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+        Components components = new Components()
+                .addSecuritySchemes(securityJwtName, new SecurityScheme()
+                        .name(securityJwtName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme(BEARER_TOKEN_PREFIX)
+                        .bearerFormat(securityJwtName));
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .addServersItem(new Server().url("/product-service"))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("MSA - PRODUCT SERVICE 문서")
+                .description("00 API 테스트를 위한 Swagger UI")
+                .version("1.0.0");
+    }
+
+}
